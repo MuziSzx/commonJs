242ebd6f8d62114f0f661cf904d31e8f7eb0f263
shanzx <425002410@qq.com>
2019-04-03 15:54:32 +0800

    添加数组排序或者对象排序方法

--- common.js
+++ common.js
@@ -151,3 +151,113 @@ function formatMoney(s, type) {
     }
     return s;
 }
+
+/**
+ * 排序数组或者对象
+ * @param object [必须] 数组或对象
+ * @param subkey [可选] 需要排序的子键, 该参数可以是字符串, 也可以是一个数组
+ * @param desc   [可选] 排序方式, true:降序, false|undefined:升序
+ * @returns 返回排序后的数组或者对象
+ * 注意: 对于对象的排序, 如果使用console.log打印对象的显示可能和排序结果不一致,
+ *  其键会被浏览器以字母顺序排序显示,但在for循环中则为正确的排序顺序
+ */
+function sortObject(object, subkey, desc) {
+    if (!isNullOrEmpty(object))
+        object = JSON.parse(JSON.stringify(object));
+    var is_array = false;
+    if (Object.prototype.toString.call(object) === '[object Array]') {
+        is_array = true;
+    }
+    if (is_array) {
+        var keys = {
+            length: object.length
+        };
+    } else {
+        if (typeof (Object.keys) == 'function') {
+            var keys = Object.keys(object);
+        } else {
+            var keys = [];
+            for (var key in keys) {
+                keys.push(key);
+            }
+        }
+    }
+    for (var i = 0; i < keys.length; i++) {
+        for (var j = i + 1; j < keys.length; j++) {
+            if (is_array) {
+                //数组排序
+                if (Object.prototype.toString.call(subkey) === '[object Array]') {
+                    var vali = object[i];
+                    var valj = object[j];
+                    for (var si = 0; si < subkey.length; si++) {
+                        vali = vali[subkey[si]];
+                        valj = valj[subkey[si]];
+                    }
+                } else {
+                    if ((!subkey && subkey !== '') || subkey == '' && object.sort) {
+                        var vali = object[i];
+                        var valj = object[j];
+                    } else {
+                        var vali = object[i][subkey];
+                        var valj = object[j][subkey];
+                    }
+                }
+                if (desc) {
+                    if (valj > vali) {
+                        var tmp = object[i];
+                        object[i] = object[j];
+                        object[j] = tmp;
+                    }
+                } else {
+                    if (valj < vali) {
+                        var tmp = object[i];
+                        object[i] = object[j];
+                        object[j] = tmp;
+                    }
+                }
+            } else {
+                //对象排序
+                var obi = object[keys[i]];
+                var obj = object[keys[j]];
+                if (Object.prototype.toString.call(subkey) === '[object Array]') {
+                    var vali = obi;
+                    var valj = obj;
+                    for (var si = 0; si < subkey.length; si++) {
+                        vali = vali[subkey[si]];
+                        valj = valj[subkey[si]];
+                    }
+                } else {
+                    if ((!subkey && subkey !== '') || subkey == '' && object.sort) {
+                        var vali = obi;
+                        var valj = obj;
+                    } else {
+                        var vali = obi[subkey];
+                        var valj = obj[subkey];
+                    }
+                }
+                if (desc) {
+                    if (valj > vali) {
+                        var tmp = keys[i];
+                        keys[i] = keys[j];
+                        keys[j] = tmp;
+                    }
+                } else {
+                    if (valj < vali) {
+                        var tmp = keys[i];
+                        keys[i] = keys[j];
+                        keys[j] = tmp;
+                    }
+                }
+            } //is!array
+        }
+    }
+    if (is_array) {
+        return object;
+    } else {
+        var sorted = {};
+        for (var i = 0; i < keys.length; i++) {
+            sorted[keys[i]] = object[keys[i]];
+        }
+        return sorted;
+    }
+}
